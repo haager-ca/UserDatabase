d382c034c53c7eef5579a0fd4c173a3611364910
Caius Haager <caius.haager@icloud.com>
2023-12-23 23:25:03 +0100

    added background image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,5 @@
     import Navbar from './navbar.svelte'
 </script>
 
+
 <Navbar />
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
