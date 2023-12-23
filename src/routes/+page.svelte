a1ba0fa8c62da34363f898118a79939b5818305a
Caius Haager <caius.haager@icloud.com>
2023-12-23 22:36:07 +0100

    added bootstrap

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+    import 'bootstrap/dist/css/bootstrap.min.css'
+    import Navbar from './navbar.svelte'
+</script>
+
+<Navbar />
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
