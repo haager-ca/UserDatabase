460340b96db09c76ac43e5a63cbd64caed8a3015
Caius Haager <caius.haager@icloud.com>
2023-12-24 15:55:52 +0100

    added some inputs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,34 @@
 <script>
 	import 'bootstrap/dist/css/bootstrap.min.css';
+
+  function redirektToDelete() {
+    window.location.href = "/DeleteData"
+  }
+
+  function redirektToEdit() {
+    window.location.href = "/EditData"
+  }
+
+  function redirektToAdd() {
+    window.location.href = "/AddData"
+  }
 </script>
 
 	<main>
 		<div class="card m-3">
-			<div class="card-body">
-        <h1>Hallo, wie gehts?</h1>
+			<div class="card-body text-center">
+        <h1 class="text-center">Die genaueste Datenbank</h1>
+        <p>Diese Web-Application ist dazu da die verschiedensten Informationen zu speichern.</p>
+        <p>Sie können:</p>
+
+        <button on:click={redirektToAdd} class="m-1 btn btn-outline-success">Daten hinzufügen</button>
+        <button on:click={redirektToEdit} class="m-1 btn btn-outline-primary">Daten bearbeiten</button>
+        <button on:click={redirektToDelete} class="m-1 btn btn-outline-danger">Daten löschen</button>
+
+        <p class="m-1">Genau zu diesem Zeitpunkt umfasst die Datenbank ... Daten!!!</p>
+        <p>Sie können auch oben in der Navbar die verschiedenen Seiten ansteueren.</p>
+        <p>Außerdem gibt es oben rechts eine Suchleiste inder sie die verschiedensten Datensätze suchen können.</p>
+        <p><strong>!!!Achten sie immer auf eine korrekte Schreibweise, da sie sonnst entweder falsche Daten bekommen oder einspeichern!!!</strong></p>
 			</div>
 		</div>
 	</main>
--- src/routes/AddData/+page.svelte
+++ src/routes/AddData/+page.svelte
@@ -1,7 +1,41 @@
 <main>
 	<div class="card m-3">
 		<div class="card-body">
-			<h1>Hier kann etwas hinzugefügt werden</h1>
+			<h1 class="text-center">Daten einfügen</h1>
+			<div class="row m-3">
+				<div class="col-5">
+					<h4 class="text-center">Allgemeine Daten zu Person</h4>
+					<div class="input-group">
+						<span class="input-group-text">First and last name</span>
+						<input type="text" id="InputFirstname" aria-label="First name" class="form-control" />
+						<input type="text" id="InputLastname" aria-label="Last name" class="form-control" />
+					</div>
+					<div class="mt-3">
+						<label for="InputEmail1" class="form-label">Email address (optional)</label>
+						<input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="max.mustermann@gmail.com"/>
+					</div>
+                    <div class="mt-3">
+						<label for="Birthday" class="form-label">Geburtsdatum</label>
+						<input type="date" class="form-control" id="InputBirthday" aria-describedby="emailHelp"/>
+					</div>
+                    <div class="mt-3">
+						<label for="exampleInputEmail1" class="form-label">Email address (optional)</label>
+						<input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" placeholder="max.mustermann@gmail.com"/>
+					</div>
+				</div>
+                <div class="col-1"></div>
+				<div class="col-6">
+					<h4 class="text-center">Beschreibung der Person (Stichpunkte)</h4>
+                    <div class="form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 140px"></textarea>
+                        <label for="floatingTextarea">Beschreibung</label>
+                      </div>
+                      <div class="form-floating">
+                        <textarea class="mt-3 form-control" placeholder="Leave a comment here" id="floatingTextarea" style="height: 140px"></textarea>
+                        <label for="floatingTextarea">Eigenschaften</label>
+                      </div>
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
